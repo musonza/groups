9aab6ab941cea2c65d49f471706c288c04d15863
mathisloge <mathisloge@gmail.com>
2017-02-05 10:43:37 +0100

    Update Like.php

--- src/Models/Like.php
+++ src/Models/Like.php
@@ -6,5 +6,12 @@ use Eloquent;
 
 class Like extends Eloquent
 {
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'group_likes';
+    
     protected $fillable = ['user_id'];
 }
