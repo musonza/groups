6986f084f88c1a3790c04e1d72bccc19b01eb2d0
mathisloge <mathisloge@gmail.com>
2017-02-05 10:23:37 +0100

    Update 2016_03_26_032730_create_posts_table.php
    To avoid confilct with other tabels

--- src/migrations/2016_03_26_032730_create_posts_table.php
+++ src/migrations/2016_03_26_032730_create_posts_table.php
@@ -12,7 +12,7 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('group_posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
             $table->text('body');
