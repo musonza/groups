4c0f1a9a375c4727c54b5428a0795e9ee16ac0ca
mathisloge <mathisloge@gmail.com>
2017-02-05 10:44:53 +0100

    Update Report.php

--- src/Models/Report.php
+++ src/Models/Report.php
@@ -6,5 +6,12 @@ use Eloquent;
 
 class Report extends Eloquent
 {
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'group_reports';
+    
     protected $fillable = ['user_id'];
 }
