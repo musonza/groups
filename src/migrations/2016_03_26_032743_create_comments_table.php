e9264898598784e170347f11d873efe2b50a9d85
mathisloge <mathisloge@gmail.com>
2017-02-05 10:42:16 +0100

    Update 2016_03_26_032743_create_comments_table.php

--- src/migrations/2016_03_26_032743_create_comments_table.php
+++ src/migrations/2016_03_26_032743_create_comments_table.php
@@ -12,7 +12,7 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('group_comments', function (Blueprint $table) {
             $table->increments('id');
             $table->string('body');
             $table->integer('user_id')->unsigned();
