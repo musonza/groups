097bec8634615b366e96e1af07003f6a33c10d83
Tinashe Musonza <tinashe@Tinashes-MacBook-Pro.local>
2017-10-23 14:58:30 -0500

    add mysql to travis

--- tests/config/database.php
+++ tests/config/database.php
@@ -2,18 +2,40 @@
 
 return [
 
-    'fetch' => PDO::FETCH_CLASS,
+    //...
 
-    'default' => 'sqlite',
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    //...
 
     'connections' => [
 
-        'sqlite' => [
-            'driver'   => 'sqlite',
-            'database' => __DIR__.'/../database/testing.sqlite',
-            'prefix'   => '',
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'homestead'),
+            'username'  => env('DB_USERNAME', 'homestead'),
+            'password'  => env('DB_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'username'  => env('DB_TEST_USERNAME', 'homestead'),
+            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
         ],
 
     ],
+
+    //...
 
 ];
